56d26d5f5ec3facba66a3810dd34a8ea80a429c3
Marcelo <tchellinho200@gmail.com>
2025-02-13 17:15:17 -0300

    final corrections

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'standalone',
+    trailingSlash: false, // Certifique-se de que está como false
+    async redirects() {
+      return [
+        {
+          source: '/404.html',
+          destination: '/',
+          permanent: false, // Define que o redirecionamento é temporário
+        },
+      ]
+    }
+  }
+  
 
 export default nextConfig;
